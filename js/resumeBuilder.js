bb246f1d2256ebc9a5c362115e1852fc3cdd085a
Giacomo Napoli <giacomonapoli@gmail.com>
2016-04-18 00:56:22 +0200

    Finish Problem Set 0

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ $("#main").append(["Giacomo Napoli"]);
+
+ /*QuIZ speed of light
+ speed_of_light_ms = 299792458;
+ speed_of_light_cn = speed_of_light_ms*100.0/1000000000.0;
+
+ $("#main").append([speed_of_light_cn]);
+*/
